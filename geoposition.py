c52fda4a0228684e7493a66edca011aeaa330abf
Rassafel <48612719+Rassafel@users.noreply.github.com>
2020-06-21 08:10:24 +0400

    Add files via upload

---
+++ geoposition.py
@@ -0,0 +1,36 @@
+import googlemaps
+
+token_file = r'googleApi.txt'
+
+
+class GoogleMap:
+    def __init__(self):
+        api_key = ''
+        with open(file=token_file, mode='r', encoding='utf-8') as f:
+            api_key = f.readline()
+        self.gmaps = googlemaps.Client(key=api_key)
+
+    def distance(self, location1: dict, location2: dict):
+        return \
+            self.gmaps.distance_matrix(location1, location2,
+                                       language='ru')[
+                'rows'][
+                0][
+                'elements'][0]['distance'][
+                'value'] / 1000000
+
+    def location_to_point(self, location: str):
+        return \
+            self.gmaps.geocode(location, language='ru')[0][
+                'geometry']['location']
+
+    def point_to_location(self, point: dict):
+        return \
+            self.gmaps.reverse_geocode(point,
+                                       language='ru')[0][
+                'formatted_address']
+
+    def location_point_from_text(self, location:str):
+        result = self.gmaps.geocode(location, language='ru')[0]
+        return result['formatted_address'], \
+               result['geometry']['location']
